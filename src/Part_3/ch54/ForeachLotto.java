0e083b919051a5bcad563d62d34b9d7244c6a335
beautiflow <1114ny@naver.com>
2022-12-15 20:54:42 +0900

    20221214

---
+++ src/Part_3/ch54/ForeachLotto.java
@@ -0,0 +1,12 @@
+package Part_3.ch54;
+
+public class ForeachLotto {
+    public static void main(String[] args) {
+
+        int[] mm = {1,6,16,22,23,33};
+        for(int m: mm){
+            System.out.println(m + "\t");
+        }
+        System.out.println();
+    }
+}
